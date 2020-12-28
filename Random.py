ce530b6c6feddd2501545d083f3394fadafa755e
Wilmer Chau <wilmer.chau@gmail.com>
2020-12-28 07:10:18 -0500

    Added error counter

--- Random.py
+++ Random.py
@@ -51,9 +51,18 @@
 
 problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 
+
+out_line1=""
+out_line2=""
+out_line3=""
+out_line4=""
+error_counter=0
+
 if len(problems) > 5:
     print ("Error: Too many problems.")
+
 else:
+    
     for problem in problems:
         calculations = problem.split()
         # print(calculations)
@@ -62,12 +71,10 @@ else:
         num2 = int(calculations[-1])
         operation = calculations[1]
 
-        out_line1=""
-        out_line2=""
-        out_line3=""
-        out_line4=""
+        
         max_width=0
-        separator=4
+     
+        separator="    "
         
         #Check if both numbers are integers
         if ((type(num1) == int) and (type(num2) == int)):
@@ -92,34 +99,117 @@ else:
                         line1_space_counter = max_width - len(calculations[0])
                         # print(max_width)
                         # print(line1_space_counter)
-                        while line1_space_counter > 0:
-                            out_line1 += " "
-                            line1_space_counter -= 1
-                        out_line1 += str(calculations[0])
+                        if out_line1 =="":
+                            while line1_space_counter > 0:
+                                out_line1 += " "
+                                line1_space_counter -= 1
+                            out_line1 += str(calculations[0])
+                        else:
+                            while line1_space_counter > 0:
+                                out_line1 += " "
+                                line1_space_counter -= 1
+                            out_line1 += separator + str(calculations[0])
                         
-                        
-                        out_line2 += operation
-                        line2_space_counter = max_width - 1 - len(calculations[-1])
-                        while line2_space_counter > 0:
-                            out_line2 += " "
-                            line2_space_counter -= 1
-                        out_line2 += str(calculations[-1])
-                        
-                        line3_space_counter = 0
-                        while line3_space_counter < max_width:
-                            out_line3 += "-"
-                            line3_space_counter += 1
+                        if out_line2 == "":
+                            out_line2 += operation
+                            line2_space_counter = max_width - 1 - len(calculations[-1])
+                            while line2_space_counter > 0:
+                                out_line2 += " "
+                                line2_space_counter -= 1
+                            out_line2 +=  str(calculations[-1])
+                        else:
+                            out_line2 += separator + operation
+                            line2_space_counter = max_width - 1 - len(calculations[-1])
+                            while line2_space_counter > 0:
+                                out_line2 += " "
+                                line2_space_counter -= 1
+                            out_line2 += str(calculations[-1])
 
+                        if out_line3 == "":
+                            line3_space_counter = 0
+                            while line3_space_counter < max_width:
+                                out_line3 += "-"
+                                line3_space_counter += 1
+                        else:
+                            out_line3 += separator
+                            line3_space_counter = 0
+                            while line3_space_counter < max_width:
+                                out_line3 += "-"
+                                line3_space_counter += 1
+                            
+                        if out_line4 == "":
+                            line4_space_counter = 0
+                            line4_space_counter = max_width - len(str(total)) 
+                            while line4_space_counter > 0:
+                                out_line4 += " "
+                                line4_space_counter -= 1
+                            out_line4 += str(total)
+                        else:
+                            out_line4 += separator
+                            line4_space_counter = max_width - len(str(total)) 
+                            while line4_space_counter > 0:
+                                out_line4 += " "
+                                line4_space_counter -= 1
+                            out_line4 += str(total)
 
-                        print(out_line1)
-                        print(out_line2)
-                        print(out_line3)
-
-
-
-                    else:
+                    
+                    else: # num2 > num1
                         max_width = len(calculations[-1]) + 2
-                
+                        line1_space_counter = max_width - len(calculations[0])
+                        # print(max_width)
+                        # print(line1_space_counter)
+                        if out_line1 =="":
+                            while line1_space_counter > 0:
+                                out_line1 += " "
+                                line1_space_counter -= 1
+                            out_line1 += str(calculations[0])
+                        else:
+                            while line1_space_counter > 0:
+                                out_line1 += " "
+                                line1_space_counter -= 1
+                            out_line1 += separator + str(calculations[0])
+                        
+                        if out_line2 == "":
+                            out_line2 += operation
+                            line2_space_counter = max_width - 1 - len(calculations[-1])
+                            while line2_space_counter > 0:
+                                out_line2 += " "
+                                line2_space_counter -= 1
+                            out_line2 +=  str(calculations[-1])
+                        else:
+                            out_line2 += separator + operation
+                            line2_space_counter = max_width - 1 - len(calculations[-1])
+                            while line2_space_counter > 0:
+                                out_line2 += " "
+                                line2_space_counter -= 1
+                            out_line2 += str(calculations[-1])
+
+                        if out_line3 == "":
+                            line3_space_counter = 0
+                            while line3_space_counter < max_width:
+                                out_line3 += "-"
+                                line3_space_counter += 1
+                        else:
+                            out_line3 += separator
+                            line3_space_counter = 0
+                            while line3_space_counter < max_width:
+                                out_line3 += "-"
+                                line3_space_counter += 1
+
+                        if out_line4 == "":
+                            line4_space_counter = 0
+                            line4_space_counter = max_width - len(str(total)) 
+                            while line4_space_counter > 0:
+                                out_line4 += " "
+                                line4_space_counter -= 1
+                            out_line4 += str(total)
+                        else:
+                            out_line4 += separator
+                            line4_space_counter = max_width - len(str(total)) 
+                            while line4_space_counter > 0:
+                                out_line4 += " "
+                                line4_space_counter -= 1
+                            out_line4 += str(total)
                 
                 else:
                     print("Error: Operator must be '+' or '-'.")
@@ -127,7 +217,8 @@ else:
         else:
             print("Error: Numbers must only contain digits.")
             break
-
+out_total = out_line1 + "\n" + out_line2 + "\n" + out_line3 + "\n" + out_line4
+print(out_total)
 
 
 
